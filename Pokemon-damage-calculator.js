51bde075eda2aa306d47c723b2a7c06774840a71
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-02-06 19:25:46 -0800

    Update Pokemon-damage-calculator.js

--- Pokemon-damage-calculator.js
+++ Pokemon-damage-calculator.js
@@ -40,3 +40,14 @@ Also, assume that the relationships between different types are symmetric
 */
 
 // Solution
+
+function calculateDamage(yourType, opponentType, attack, defense){
+  let effectivenessMap = {
+  'fire':{'fire':0.5,'grass':2,'water':0.5,'electric':1}, 
+  'grass':{'fire':0.5,'grass':0.5,'water':2,'electric':1}, 
+  'water':{'fire':2,'grass':0.5,'water':0.5,'electric':0.5},
+  'electric':{'fire':1,'grass':1,'water':2,'electric':0.5}
+  };
+  
+  return Math.ceil(50 * (attack/defense) * effectivenessMap[yourType][opponentType]);
+}
